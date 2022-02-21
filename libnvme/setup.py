0233cd25a2d57bdd7a9079213f0bb063ef478e5c
Boris Glimcher <Boris.Glimcher@emc.com>
2022-02-21 13:03:12 +0200

    SWIG: remove explicit invocation
    python distutils handles swig already, so no need to
    explicitely define it in meson build
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- libnvme/setup.py
+++ libnvme/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 libnvme_module = Extension(
     '_nvme',
-    sources = ['nvme_wrap.c'],
+    sources = ['nvme.i'],
     libraries = ['nvme', 'json-c', 'uuid'],
     library_dirs = ['../src'],
     include_dirs = ['../ccan', '../src', '../src/nvme'],
