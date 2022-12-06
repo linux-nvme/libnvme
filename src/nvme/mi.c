30dd19802440a69ac2218788ff4025ae081dac84
Hao Jiang <jianghao@google.com>
2022-12-05 16:48:35 -0800

    mi: fix the return error code.
    When returning a system error code (i.e. rc < 0), the function should
    return -1 and set errno instead of returning the errno directly. It is
    very helpful for the client to handle the rc in an unified way.
    
    Signed-off-by: Hao Jiang <jianghao@google.com>

--- src/nvme/mi.c
+++ src/nvme/mi.c
@@ -1055,7 +1055,10 @@ int nvme_mi_admin_get_features(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_get_features);
@@ -1094,7 +1097,10 @@ int nvme_mi_admin_set_features(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_set_features);
@@ -1138,7 +1144,10 @@ int nvme_mi_admin_ns_mgmt(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_ns_mgmt);
@@ -1174,7 +1183,10 @@ int nvme_mi_admin_ns_attach(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_ns_attach);
@@ -1207,16 +1219,28 @@ int nvme_mi_admin_fw_download(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	if (args->data_len & 0x3)
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	if (args->offset & 0x3)
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	if (!args->data_len)
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_fw_download);
@@ -1249,7 +1273,10 @@ int nvme_mi_admin_fw_commit(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_fw_commit);
@@ -1279,7 +1306,10 @@ int nvme_mi_admin_format_nvm(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_format_nvm);
@@ -1313,7 +1343,10 @@ int nvme_mi_admin_sanitize_nvm(nvme_mi_ctrl_t ctrl,
 	int rc;
 
 	if (args->args_size < sizeof(*args))
-		return -EINVAL;
+	{
+		errno = EINVAL;
+		return -1;
+	}
 
 	nvme_mi_admin_init_req(&req, &req_hdr, ctrl->id,
 			       nvme_admin_sanitize_nvm);
