b7b57602ae424135f038821ba87ee9f2cc6d9b13
Tokunori Ikegami <ikegami.t@gmail.com>
2023-11-12 23:24:05 +0900

    build: Add -std=c99 and delete libcurl, libarchive and libpkgconf for muon
    The changes required as same with the nvme-cli build for muon.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -143,13 +143,10 @@ tools_build_muon() {
         "${BUILDDIR}/build-tools/muon"
     pushd "${BUILDDIR}/build-tools/muon" || exit 1
 
-    CC="${CC}" ninja="${SAMU}" ./bootstrap.sh stage1
+    CC="${CC}" CFLAGS="${CFLAGS} -std=c99" ninja="${SAMU}" ./bootstrap.sh stage1
 
     CC="${CC}" ninja="${SAMU}" stage1/muon setup        \
         -Dprefix="${BUILDDIR}/build-tools"              \
-        -Dlibcurl=enabled                               \
-        -Dlibarchive=enabled                            \
-        -Dlibpkgconf=enabled                            \
         -Ddocs=disabled                                 \
         -Dsamurai=disabled                              \
         "${BUILDDIR}/build-tools/.build-muon"
