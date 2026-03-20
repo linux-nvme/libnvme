64a0208f09e2ade0198ef5676781992819dfb667
Martin George <marting@netapp.com>
2026-03-20 22:47:43 +0530

    tree: avoid updating --tls in nvme_read_sysfs_tls()
    It is wrongly assumed that the presence of the sysfs tls_key
    attribute indicates --tls alone was invoked. But this can
    also happen if --concat was invoked as well. And both --tls
    and --concat are mutually exclusive. Also, both --tls and
    --concat are already appropriately set earlier during configured
    & generated PSK TLS workflows respectively. So avoid explicitly
    setting --tls again here in nvme_read_sysfs_tls() as that's
    unnecessary and incorrect too.
    
    Signed-off-by: Martin George <marting@netapp.com>
    (cherry picked from f1818da3448f)
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- src/nvme/tree.c
+++ src/nvme/tree.c
@@ -2013,10 +2013,9 @@ static void nvme_read_sysfs_tls(nvme_root_t r, nvme_ctrl_t c)
 
 	key = nvme_get_ctrl_attr(c, "tls_key");
 	if (!key) {
-		/* tls_key is only present if --tls has been used. */
+		/* tls_key is only present if --tls or --concat has been used */
 		return;
 	}
-	c->cfg.tls = true;
 
 	keyring = nvme_get_ctrl_attr(c, "tls_keyring");
 	nvme_ctrl_set_keyring(c, keyring);
