linux-nvme/nvme-cli

fabrics: add error if no dhchap-secret is specified with --concat
--concat requires a corresponding dhchap-secret key to be passed
with it. So add an appropriate error message if this is not done.

Signed-off-by: Martin George <marting@netapp.com>

libnvme/src/nvme/fabrics.c
@@ -737,6 +737,11 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 		return -ENVME_CONNECT_INVAL;
 	}
 
+	if (cfg->concat && !hostkey) {
+		nvme_msg(h->ctx, LOG_ERR, "required argument [--dhchap-secret | -S] not specified with --concat\n");
+		return -ENVME_CONNECT_INVAL;
+	}
+
 	if (cfg->tls) {
 		ret = __nvme_import_keys_from_config(h, c, &keyring_id, &key_id);
 		if (ret)

linux-nvme/libnvme

(cherry picked from 62a3ec111d5d)
Signed-off-by: Daniel Wagner <wagi@kernel.org>

src/nvme/fabrics.c
@@ -632,6 +632,11 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 		return -ENVME_CONNECT_INVAL;
 	}
 
+	if (cfg->concat && !hostkey) {
+		nvme_msg(h->r, LOG_ERR, "required argument [--dhchap-secret | -S] not specified with --concat\n");
+		return -ENVME_CONNECT_INVAL;
+	}
+
 	if (cfg->tls) {
 		ret = __nvme_import_keys_from_config(h, c, &keyring_id, &key_id);
 		if (ret) {